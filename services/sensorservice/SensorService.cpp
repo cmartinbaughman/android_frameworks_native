4e542aaa3040b84792953864bc9325258ae38d3f
Rudolf Tammekivi <rtammekivi@gmail.com>
2013-02-02 15:30:52 +0200

    merges

--- services/sensorservice/SensorService.cpp
+++ services/sensorservice/SensorService.cpp
@@ -48,6 +48,9 @@
 #include "RotationVectorSensor2.h"
 #include "SensorFusion.h"
 #include "SensorService.h"
+#include "legacy/LegacyGravitySensor.h"
+#include "legacy/LegacyLinearAccelerationSensor.h"
+#include "legacy/LegacyRotationVectorSensor.h"
 
 #ifdef USE_LEGACY_SENSORS_FUSION
 #include "legacy/LegacyGravitySensor.h"
@@ -126,14 +129,12 @@ void SensorService::onFirstRef()
                 registerVirtualSensor( new OrientationSensor() );
                 registerVirtualSensor( new CorrectedGyroSensor(list, count) );
             }
-#ifdef USE_LEGACY_SENSORS_FUSION
             else
             {
                 registerVirtualSensor( new LegacyRotationVectorSensor() );
                 registerVirtualSensor( new LegacyGravitySensor(list, count) );
                 registerVirtualSensor( new LegacyLinearAccelerationSensor(list, count) );
             }
-#endif
 
             // build the sensor list returned to users
             mUserSensorList = mSensorList;
